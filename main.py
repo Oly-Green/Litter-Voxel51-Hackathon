48b47ac37068d64f37ac642f42e1b015f29d6e91
SmokingSpy <Olivergreen104@gmail.com>
2024-09-20 13:03:28 -0400

    Classification Catergories added

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-# This is a sample Python script.
+THREAT_LEVELS = {4: ["styrofoam"], 3: ["plastic"], 2: ["glass"], 1:["paper"]}
+# threat_levels = {4: ["styrofoam"]}
+# super_cat_lists_consolidated = {"styrofoam": [57]}
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import torchvision
+import json
+import os
+from tqdm import tqdm
 
+def parse_supercategory(file_path):
+    if os.path.isfile(file_path):
+        with open(file_path, 'r', encoding='utf-8') as f:
+            annotations_dict = json.load(f)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    super_cat_lists = {}
 
+    for cat in annotations_dict["categories"]:
+        if not cat["supercategory"] in super_cat_lists:
+            super_cat_lists[cat["supercategory"].lower()] = [cat["id"]]
+        else:
+            super_cat_lists[cat["supercategory"]].append(cat["id"])
 
-# Press the green button in the gutter to run the script.
+    keywords = ["styrofoam", "plastic", "glass", "paper"]
+    super_cat_lists_consolidated = {}
+
+    for cat_name in super_cat_lists.keys():
+        for word in keywords:
+            if word in cat_name:
+                if word not in super_cat_lists_consolidated.keys():
+                    super_cat_lists_consolidated[word] = super_cat_lists[cat_name]
+                else:
+                    super_cat_lists_consolidated[word].extend(super_cat_lists[cat_name])
+
+    return super_cat_lists_consolidated
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    file_name = "annotations_unofficial.json"
+    path = os.path.join("C:/Users/olive/Documents/NortheasternDocuments/Voxel51-Hackathon/TACO/data", file_name)
+    super_cats = parse_supercategory(path)
+    print(super_cats)
